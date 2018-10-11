4be19063e28ea33d38479d9adfd71f2b9975163a
klchai <chaikelun@gmail.com>
2018-10-11 22:52:53 +0200

    Partiel 2017

--- L3/Partiel/p2017.ml
+++ L3/Partiel/p2017.ml
@@ -28,3 +28,35 @@ let rec foo f x =
     foo (fun y -> f (x + y)) (x-1)
 
 (* Ex 3 *)
+(* Q5 *)
+let intersection l1 l2 = 
+  (* let rec mem x l = 
+    match l with
+      | [] -> false
+      | head::tail ->head = x || mem x tail
+  in *)
+  let rec aux l1 l2 = 
+    match l1 with
+      | [] -> []
+      | head::tail -> if List.mem head l2 then head::(aux tail l2) else aux tail l2
+  in
+aux l1 l2
+
+let inter l1 l2 = List.filter (fun x -> List.mem x l2) l1
+
+(* Q6 *)
+let duplique l = 
+  let rec aux l acc = 
+    match l with
+      | [] -> List.rev acc
+      | head::tail -> aux tail (head::head::acc)
+  in
+aux l []
+
+(* Q7 *)
+let nombre_paire_d_elements l = 
+  let i = List.length l in
+  if i mod 2 == 0 then true
+  else false
+
+(* Q8 *)
