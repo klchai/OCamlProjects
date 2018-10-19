34fb0d0f8cf639dd03b8ff8cab9428383c216dd0
klchai <chaikelun@gmail.com>
2018-10-19 12:04:56 +0200

    TP 4

--- L3/TP4/tp4.ml
+++ L3/TP4/tp4.ml
@@ -12,10 +12,40 @@ let t0 = [|
     [| black ; black ; black ; black |] ;
     [| black ; black ; black ; black |]
   |]
-
-
-
+(* 
 let () =
   affiche a0;
-  ignore(Graphics.read_key())
+  ignore(Graphics.read_key()) *)
            
+let rec get_pixel x y longueur arbre = 
+  match arbre with
+    | F(c) -> c
+    | N(so,se,no,ne) -> 
+        let h = longueur/2 in
+          if x<h && y<h then get_pixel x y h so
+          else if x>=h && y<h then get_pixel (x-h) y h se
+          else if x<h && y>=h then get_pixel x (y-h) h no
+          else get_pixel (x-h) (y-h) h ne
+;;
+let () = Printf.printf "%x" (get_pixel 2 1 4 a0);;
+let () = Printf.printf "\n%x" (get_pixel 0 0 4 a0);;
+
+let image_matrix_of_tree l t = 
+  Array.init l (fun x->
+    Array.init l (fun y ->
+      get_pixel x y l t
+      )
+    )
+
+let monochrome_color image_matrix x y longueur couleur = 
+  let exception Diff in
+  try
+    for x = x to x+longueur-1 do
+       for y = y to y+longueur-1 do
+         if image_matrix.(x).(y) <> couleur then
+           raise Diff
+         done
+      done;
+    true
+  with Diff -> false
+;;
