73448c19b59a8ee0907e6e0c6a194f1f51cf6461
klchai <chaikelun@gmail.com>
2018-10-26 16:19:34 +0200

    TP5

--- L3/TP5/t9.ml
+++ L3/TP5/t9.ml
@@ -1,7 +1,7 @@
 type t = { mots : string list ; branches : (int * t) list }
 
 (* Q1 *)
-let empty = {mots = []; branches = []}
+let empty = { mots = []; branches = [] }
 
 (* Q2 *)
 let rec find l t = 
@@ -15,7 +15,7 @@ let rec change_assoc x v l =
     | [] -> [(x,v)]
     | (x',v') ::s -> if x = x' then (x,v)::s
                      else (x',v')::change_assoc x v s
-;;
+
 (* Q4 *)
 let rec add l m a = 
   match l with
